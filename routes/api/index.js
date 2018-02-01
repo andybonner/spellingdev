cdc0a216fc9d0b6f2f5051eaac4f07d7d4051a8d
Andy Bonner <abonner@duke.edu>
2018-02-01 11:13:12 -0500

    API routes index

---
+++ routes/api/index.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+// const wordRoutes = require("./words");
+// const listRoutes = require("./lists");
+
+// Word routes
+// router.use("/words", wordRoutes);
+
+// List routes
+// router.use("/lists", listRoutes);
+
+module.exports = router;
