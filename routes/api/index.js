67abcceb3394dd16fde5815a8dabd678c2eea989
Andy Bonner <abonner@duke.edu>
2018-02-01 15:12:32 -0500

    comment

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,13 @@
+// Direct routes starting with "/api" into routes for word or list actions
+
 const router = require("express").Router();
-// const wordRoutes = require("./words");
-// const listRoutes = require("./lists");
+const wordRoutes = require("./words");
+const listRoutes = require("./lists");
 
 // Word routes
-// router.use("/words", wordRoutes);
+router.use("/words", wordRoutes);
 
 // List routes
-// router.use("/lists", listRoutes);
+router.use("/lists", listRoutes);
 
 module.exports = router;
