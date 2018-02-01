4886978b3678a44e7c80376076e405a92d3aa307
Andy Bonner <abonner@duke.edu>
2018-02-01 16:20:59 -0500

    pseudocoding

--- routes/api/words.js
+++ routes/api/words.js
@@ -4,7 +4,14 @@ const wordsController = require("../../controllers/wordsController");
 // Matches with "/api/words"
 router.route("/")
   .get(wordsController.findAll)
-  .post(wordsController.create);
+  .post(
+    // in here:
+    // 1. instead of immediately invoking wordsController.create--which is, in fact, a function with req and res params--turn this into such a function.
+    // 2. take the req.body.word and send it off as a query to OED
+    // 3. pad OED's data into req.body, under properties audio_path and example
+    // 4. simply submit wordsController.create(req). Original HTTP req data will still be there, and wordsController will take care of the rest.
+    wordsController.create
+  );
 
 // Matches with "/api/words/:id"
 router
