16379b3729a1d8265092feb87698e61acc42f8d1
Andy Bonner <abonner@duke.edu>
2018-02-02 08:29:18 -0500

    return promise

--- routes/oed.js
+++ routes/oed.js
@@ -1,1 +1,17 @@
 // Route to fetch word data from external Oxford English Dictionary API
+const request = require('request');
+
+const headers = {
+  "Accept": "application/json",
+  "app_id": "0221faad",
+  "app_key": "e4af98008c290b83307d432a537cf190"
+}
+
+module.exports = {
+  lookup: word => {
+    return requestP({
+      uri: 'https://od-api.oxforddictionaries.com:443/api/v1/entries/en/' + word + '/regions=us',
+      headers: headers
+    });
+  }
+}
