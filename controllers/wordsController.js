314d0676c2e441bbe90aedf688c0ab8912ddadea
Andy Bonner <abonner@duke.edu>
2018-02-17 09:09:00 -0500

    add skeleton of insertMany method

--- controllers/wordsController.js
+++ controllers/wordsController.js
@@ -20,6 +20,12 @@ module.exports ={
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  insertMany: function(req, res) {
+    db.Word
+      .insertMany(req.body) //check--should it be req.body. something?
+      .then(docs => console.log(docs)) //TODO: replace this testing line with real handling
+      .catch(err => res.status(422).json(err));
+  },
   update: function(req, res) {
     db.Word
       .findOneAndUpdate({ _id: req.params.id }, req.body)
