dbc57dfb468ea5faaaeb9c47d0ef2a75eb623ac9
Andy Bonner <abonner@duke.edu>
2018-03-16 13:15:16 -0400

    use promises with oedBundle

--- services/oedBundle.js
+++ services/oedBundle.js
@@ -4,26 +4,32 @@ const jsonFind = require('json-find');
 const oedCall = require('./helpers/oedCall');
 
 const bundle = (obj) => {
-  // pass input word to oedCall to fetch OED data
-  oedCall.lookup(obj.word, (data) => {
-    const doc = jsonFind(data);
 
-    // find audio, add to obj
-    obj.audio_path = doc.findValues('audioFile').audioFile;
-
-    // find and add definitions (an array)
-    obj.definitions = doc.findValues('definitions').definitions;
-    // TODO: consider harvesting more definitions, giving the user the option to cyle through multiples.
-    // It can't be done with doc.findValues('definitions', 'definitions', 'definitions'), though.
-
-    // find and add word usage examples
-    // for prop 'examples', OED returns an array of objecs, each with prop 'text'
-    obj.examples = [];
-    const examplesArray = doc.findValues('examples').examples;
-    for (let i = 0; i < examplesArray.length; i++) {
-      const exampleObj = examplesArray[i];
-      obj.examples.push(exampleObj.text);
-    }
+  return new Promise((resolve, reject) => {
+    // pass input word to oedCall to fetch OED data
+    oedCall.lookup(obj.word, (data) => {
+      const doc = jsonFind(data);
+  
+      // find audio, add to obj
+      obj.audio_path = doc.findValues('audioFile').audioFile;
+  
+      // find and add definitions (an array)
+      obj.definitions = doc.findValues('definitions').definitions;
+      // TODO: consider harvesting more definitions, giving the user the option to cyle through multiples.
+      // It can't be done with doc.findValues('definitions', 'definitions', 'definitions'), though.
+  
+      // find and add word usage examples
+      // for prop 'examples', OED returns an array of objecs, each with prop 'text'
+      obj.examples = [];
+      const examplesArray = doc.findValues('examples').examples || [{text: "Sorry, no example available"}];
+      for (let i = 0; i < examplesArray.length; i++) {
+        const exampleObj = examplesArray[i];
+        obj.examples.push(exampleObj.text);
+      }
+  
+      console.log("Within oedBundle, after all operations, obj is:", obj);
+    });
+    resolve(obj);
   });
 }
 
--- services/wordMaker.js
+++ services/wordMaker.js
@@ -6,38 +6,50 @@ module.exports = (req, res) => {
   // extract wordlist from client req. This is an array of word strings.
   // TODO: make sure client side has performed validation to clean non-alphanumerics, split string, and spellcheck
   
+  console.log("Req.body received as", req.body);
+  
   // map array of strings to array of objects
   const wordCollection = req.body.words.map((element) => {
     const newObj = {};
     newObj.word = element;
-    // TODO: consider doing all the work here?
     return newObj;
   });
+  
+  console.log("After mapping, wordCollection is", wordCollection);
 
-  // for each obj in array, send to oedBundle for external data
-  for (let i = 0, length = wordCollection.length; i < length; i++) {
-    // pass object to oedBundle, which will pack OED data into it
-    oedBundle(wordCollection[i]);
+  async function bundleAll(array) {    
+    // for each obj in array, send to oedBundle for external data
+    for (let i = 0, length = array.length; i < length; i++) {
+      // pass object to oedBundle, which will pack OED data into it
+      oedBundle(array[i]);
+    }
+    return await Promise.all(array);
   }
-
-  // send entire array of word objects to wordsController's insertMany method
-  wordsController.insertMany(wordCollection, (dbDocs) => {
-    // dbDocs is an array of the resultant doc objects
-    // save word IDs to an array, to be added to list
-    const updateObj = {
-      words: []
-    }
-    for (let i = 0, length = wordCollection.length; i < length; i++) {
-      const wordObj = wordCollection[i];
-      updateObj.words.push(wordObj._id);
-    }
-    // add IDs of new words as refs in List
-    // req.body.listID holds ID of relevant list
-    listsController.update(req.body.listID, updateObj, (dbList) => {
-      // return updated list to the client
-      res.json(dbList);
-    })
+  
+  
+  bundleAll(wordCollection).then((alteredArray) => {
+    console.log("After bundling, alteredArray is", alteredArray);
+    console.log("After bundling, wordCollection is", wordCollection);
     
+    // send entire array of word objects to wordsController's insertMany method
+    wordsController.insertMany(wordCollection, (dbDocs) => {
+      // dbDocs is an array of the resultant doc objects
+      console.log("DB responded to words insertMany with", dbDocs);
+      // save word IDs to an array, to be added to list
+      const updateObj = {
+        words: []
+      }
+      for (let i = 0, length = dbDocs.length; i < length; i++) {
+        const wordObj = dbDocs[i];
+        updateObj.words.push(wordObj._id);
+      }
+      // add IDs of new words as refs in List
+      // req.body.listID holds ID of relevant list
+      listsController.update(req.body.listID, updateObj, (dbList) => {
+        // return updated list to the client
+        res.json(dbList);
+      });
+      
+    });
   });
-
 }
